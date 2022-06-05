e712455dd2f240a876e425eef3a07755bf7c47da
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-06-05 14:10:44 +0530

    Updated on 5th May 2022

--- app/src/main/java/com/example/services_android/MainActivity.kt
+++ app/src/main/java/com/example/services_android/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.services_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 /**
  * MainActivity
  * */
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var btnLoginActivity : Button
+    private lateinit var btnThreadActivity : Button
+
     /**
      * onCreate callback method of the Activity.
      * */
@@ -16,8 +20,25 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLoginActivity = findViewById(R.id.btnLogActivity)
+        btnThreadActivity = findViewById(R.id.btnThreadActivity)
+
         Intent(this , LogService::class.java).apply {
             startService(this)
+        }
+    }
+
+    /**
+     * onStart callback method of the Activity.
+     * */
+    override fun onStart()
+    {
+        super.onStart()
+        btnLoginActivity.setOnClickListener {
+
+        }
+        btnThreadActivity.setOnClickListener {
+
         }
     }
 }
