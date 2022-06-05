aa14b804ec5bf8d1970cd3458018d630cff29fd2
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-06-05 14:07:27 +0530

    Updated on 5th May 2022

--- app/src/main/java/com/example/services_android/MainActivity.kt
+++ app/src/main/java/com/example/services_android/MainActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * MainActivity
+ * */
 class MainActivity : AppCompatActivity()
 {
+    /**
+     * onCreate callback method of the Activity.
+     * */
     override fun onCreate(savedInstanceState : Bundle?)
     {
         super.onCreate(savedInstanceState)
